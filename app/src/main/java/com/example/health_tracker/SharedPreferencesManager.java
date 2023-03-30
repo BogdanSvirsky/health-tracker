ac24fd808c791ae84109289cb22f89b3c41f2f53
BogdanSvirsky <svirskybogdan@yandex.ru>
2023-03-31 01:09:03 +0700

    I created a shared preferences manager

---
+++ app/src/main/java/com/example/health_tracker/SharedPreferencesManager.java
@@ -0,0 +1,23 @@
+package com.example.health_tracker;
+
+import android.content.SharedPreferences;
+
+public class SharedPreferencesManager {
+    private final String MLs_KEY = "COUNT_MLs";
+    private SharedPreferences sharedPreferences;
+
+    public SharedPreferencesManager(SharedPreferences sharedPreferences) {
+        this.sharedPreferences = sharedPreferences;
+    }
+
+    public void saveMLsCount (int countMLs) {
+        int currentCount = sharedPreferences.getInt(MLs_KEY, 0);
+        sharedPreferences.edit()
+                .putInt(MLs_KEY, currentCount + countMLs)
+                .apply();
+    }
+
+    public int getMLsCount() {
+        return sharedPreferences.getInt(MLs_KEY, 0);
+    }
+}
--- app/src/main/java/com/example/health_tracker/widgets/WaterWidget.java
+++ app/src/main/java/com/example/health_tracker/widgets/WaterWidget.java
@@ -1,6 +1,7 @@
 package com.example.health_tracker.widgets;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 
 import androidx.annotation.NonNull;
@@ -10,9 +11,22 @@ import com.example.health_tracker.databinding.WaterWidgetBinding;
 
 public class WaterWidget extends CardView {
     private WaterWidgetBinding binding;
+    private int goal, currentMLs = 0;
+    private enum Cups {SmallCup, MediumCup, LargeCup}
+    private Cups currentCup = Cups.MediumCup;
 
     public WaterWidget(@NonNull Context context) {
         super(context);
         binding = WaterWidgetBinding.inflate(LayoutInflater.from(context), this);
+
+        binding.addCup.setOnClickListener(v -> {
+            switch (currentCup) {
+                case MediumCup: currentMLs += 200;
+            }
+        });
+    }
+
+    public void setGoal(int goal) {
+        this.goal = goal;
     }
 }
